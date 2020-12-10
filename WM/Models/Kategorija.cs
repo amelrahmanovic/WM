40e713667ce10da41e0b5fb24ebad5c4eb6cd5e5
Amel <amelrahmanovic@hotmail.com>
2020-12-10 13:46:57 +0100

    Models added.

---
+++ WM/Models/Kategorija.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WM.Models
+{
+    public class Kategorija
+    {
+        public int Id { get; set; }
+        public int Naziv { get; set; }
+    }
+}
---
+++ WM/Models/Proizvod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WM.Models
+{
+    public class Proizvod
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public string Opis { get; set; }
+        public int KategorijaId { get; set; }
+        public Kategorija Kategorija { get; set; }
+        public string Proizvodjac { get; set; }
+        public int DobavljacId { get; set; }
+        public Dobavljac Dobavljac { get; set; }
+        public string Cena { get; set; }
+    }
+}
